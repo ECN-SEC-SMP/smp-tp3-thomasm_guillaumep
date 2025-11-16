bcef54cbd7674097b189d594f5f2123306f9809b
Thomas Metais <thomas.metais@etu.univ-nantes.fr>
2025-11-16 22:11:15 +0100

    ajout de commentaires

--- elementStructurant.h
+++ elementStructurant.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <array>
+#include <array> // pour faire des tableaux 2D
 
 #define WIDTH 3
 
-using squareElementStructurant = array<array<bool, WIDTH>, WIDTH>;
+using squareElementStructurant = array<array<bool, WIDTH>, WIDTH>; // élément structurant carré WIDTH x WIDTH
--- main.cpp
+++ main.cpp
@@ -1,38 +1,39 @@
-#include "chargesauve.h"
-#include "outils.h"
-#include "elementStructurant.h"
+#include "chargesauve.h"        // pour les fonctions de chargement et sauvegarde d'images
+#include "outils.h"             // pour les fonctions de traitement d'images
+#include "elementStructurant.h" // pour la définition des éléments structurants
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-  bool ok = false;
-  unsigned int seuil = 155;
-  seuillage("./woman512x512.pgm", seuil, ok);
-  difference("./kodie512x512.pgm", "./lena512x512.pgm");
+    bool ok = false;                                       // variable pour vérifier le succès des opérations
+    unsigned int seuil = 155;                              // seuil pour le seuillage
+    seuillage("./woman512x512.pgm", seuil, ok);            // seuillage de l'image
+    difference("./kodie512x512.pgm", "./lena512x512.pgm"); // différence entre deux images
 
-  squareElementStructurant square1 =
-      {{
-          {0, 1, 0},
-          {1, 1, 1},
-          {0, 1, 0},
-      }};
+    squareElementStructurant square1 = // élément structurant carré 3x3
+        {{
+            {0, 1, 0},
+            {1, 1, 1},
+            {0, 1, 0},
+        }};
 
-  dilatation(square1, "./woman512x512_seuillée.pgm", ok);
-  squareElementStructurant square = {{
-      {0, 1, 0},
-      {1, 1, 1},
-      {0, 1, 0},
-  }};
-  erosion("./spriteErosion.pgm", square);
+    dilatation(square1, "./woman512x512_seuillée.pgm", ok); // dilatation avec l'élément structurant square1
+    squareElementStructurant square = {{
+        // élément structurant pour l'érosion
+        {0, 1, 0},
+        {1, 1, 1},
+        {0, 1, 0},
+    }};
+    erosion("./spriteErosion.pgm", square); // érosion avec l'élément structurant square
 
-  ouverture("./woman512x512_seuillée.pgm", square1, ok);
-  fermeture("./woman512x512_seuillée.pgm", square1, ok);
+    ouverture("./woman512x512_seuillée.pgm", square1, ok); // ouverture avec l'élément structurant square1
+    fermeture("./woman512x512_seuillée.pgm", square1, ok); // fermeture avec l'élément structurant square1
 
-  seuillage("./plane512x512.pgm", seuil, ok);
-  ouverture("./plane512x512_seuillée.pgm", square1, ok);
-  fermeture("./plane512x512_seuillée.pgm", square1, ok);
+    seuillage("./plane512x512.pgm", seuil, ok);            // seuillage de l'image plane
+    ouverture("./plane512x512_seuillée.pgm", square1, ok); // ouverture avec l'élément structurant square1
+    fermeture("./plane512x512_seuillée.pgm", square1, ok); // fermeture avec l'élément structurant square1
 
-  return 0;
+    return 0; // fin du programme
 }
